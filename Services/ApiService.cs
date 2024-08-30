60a132c1fd4c09794df5c64125bf2351f8329ce1
Thuso-Runninghill <thusok@runninghill.co.za>
2024-08-30 21:30:06 +0200

    Outline for B2c auth

---
+++ Services/ApiService.cs
@@ -0,0 +1,53 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using TrackR.Models;
+using Microsoft.Identity.Client;
+
+namespace TrackR.Services
+{
+    public class ApiService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ApiService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<bool> ExportUserDataAsync(AuthenticationResult authResult, UserData userData)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authResult.AccessToken);
+
+            var jsonContent = JsonConvert.SerializeObject(userData);
+            var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync("https://api.yourdomain.com/userdata", content);
+
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<UserData> GetUserDataAsync(AuthenticationResult authResult)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authResult.AccessToken);
+
+            var response = await _httpClient.GetAsync("https://api.yourdomain.com/userdata");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                var userData = JsonConvert.DeserializeObject<UserData>(jsonResponse);
+                return userData;
+            }
+
+            return null;
+        }
+
+        public async Task<UserData> GetUserByEmailAsync(AuthenticationResult authResult)
+        {
+            return null;
+        }
+    }
+}
---
+++ Services/AuthService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Identity.Client;
+namespace TrackR.Services
+{
+    public class AuthService
+    {
+        private readonly IPublicClientApplication _pca;
+        private readonly string[] Scopes;
+
+        public AuthService()
+        {
+            Scopes = new string[]
+            {
+                "User.Read",             // Read the user's profile information
+                "email",                 // Access the user's primary email address
+                "profile",               // Read the user's basic profile information
+                "openid"                 // Sign-in and read the user's ID token
+            };
+            _pca = PublicClientApplicationBuilder.Create("Azure-AD-Client-ID")
+                    .WithAuthority(AadAuthorityAudience.AzureAdAndPersonalMicrosoftAccount)
+                    .WithRedirectUri("Redirect-URI")
+                    .Build();
+        }
+
+        public async Task<AuthenticationResult> SignInAsync()
+        {
+            var accounts = await _pca.GetAccountsAsync();
+            AuthenticationResult result;
+            try
+            {
+                result = await _pca.AcquireTokenSilent(Scopes, accounts.FirstOrDefault())
+                                   .ExecuteAsync();
+            }
+            catch (MsalUiRequiredException)
+            {
+                result = await _pca.AcquireTokenInteractive(Scopes)
+                                   .ExecuteAsync();
+            }
+            return result;
+        }
+    }
+}
+
